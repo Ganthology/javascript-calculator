578642493ea1a17da562098102bac34c5565431e
Ganthology <gbk1386@gmail.com>
2020-12-10 23:22:26 +0800

    Create stylesheet and script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// store the first number that is input into the calculator
+// operate when = key is pressed
+// operate update the display
+// + - % x = backspace clear exponent factorial remainder squareroot 
+// digits 0 1 2 3 4 5 6 7 8 9 .
